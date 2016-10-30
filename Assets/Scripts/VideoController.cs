028fc650d1b09c3e0c1c639f9eeafc34ece3cbf2
Xavier Fischer <xavier@fischer.ninja>
2016-10-31 00:54:11 +0100

    Video scenes

---
+++ Assets/Scripts/VideoController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+/// <summary>
+/// Applies to any textured objet with a MovieTexture on it
+/// </summary>
+[RequireComponent(typeof(AudioSource))]
+public class VideoController : MonoBehaviour {
+
+	public MovieTexture movie;
+	private AudioSource audioSource;
+
+	void Start() {
+		GetComponent<RawImage>().texture = movie;
+		audioSource = GetComponent<AudioSource>();
+		audioSource.clip = movie.audioClip;
+
+		movie.Play();
+		audioSource.Play();
+	}
+
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			if (movie.isPlaying) {
+				movie.Pause();
+				audioSource.Pause();
+			} else {
+				movie.Play();
+				audioSource.Play();
+			}
+		}
+	}
+
+}
